fe56c493f12cf77e608d48846b429e2029c60d9e
lndsilva <lndsilva@hotmail.com>
2024-10-31 19:36:36 -0300

    criando listas

--- app/src/main/java/br/com/etecia/myapp/Adaptador.java
+++ app/src/main/java/br/com/etecia/myapp/Adaptador.java
@@ -1,4 +1,43 @@
 package br.com.etecia.myapp;
 
-public class Adaptador extends {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
+    private Context context;
+    private List<Carros> lstCarros;
+
+    public Adaptador(Context context, List<Carros> lstCarros) {
+        this.context = context;
+        this.lstCarros = lstCarros;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstCarros.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
